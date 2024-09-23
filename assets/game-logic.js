12ee3e409d723d4c99784477bdf6c7cb64884424
vitoriaarakaki <vitoriaakemiarakaki@gmail.com>
2024-09-23 20:19:17 -0300

    ADD Path

---
+++ assets/game-logic.js
@@ -0,0 +1,84 @@
+let words = ['Amor', 'céu', 'vento', 'livro', 'sonho', 'azul', 'noite', 'sol', 'praia', 'mesa', 'solta', 'verde', 
+    'ponte', 'fruta', 'vento', 'lume', 'canto', 'gosto', 'livro', 'vaga', 'pista', 'luz', 'tinta', 'escada', 'campo', 
+    'sorriso', 'cena', 'fogo', 'folha', 'espaço', 'ouro', 'festa', 'selo', 'faca', 'molho', 'janta', 'ponto', 'farol', 
+    'chuva', 'tela', 'ciclo', 'nota', 'chave', 'peixe', 'dia', 'prato', 'canto', 'nuvem', 'muro'];
+let chosenWord;
+let correctWord = "";
+let letters = [];
+let tries = 10;
+let gameOver = false;
+
+function pickRandomWord() {
+    chosenWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
+    drawLines();
+    return chosenWord;
+}
+
+function checkLetter(letter) {
+    if (!gameOver) {
+        if (!letters.includes(letter)) {
+            letters.push(letter);
+            if (chosenWord.includes(letter)) {
+                for (let i = 0; i < chosenWord.length; i++) {
+                    if (chosenWord[i] === letter) {
+                        drawCorrectLetter(i);
+                        addCorrectLetter(i);
+                    }
+                }
+                checkGameOver();
+                disableKey(letter, "correct");
+            } else {
+                tries--;
+                drawHangman();
+                checkGameOver();
+                disableKey(letter, "incorrect");
+            }
+        }
+    }
+}
+
+function checkGameOver() {
+    if (tries == 0) {
+        // gameover
+        showEndGameText("lose", "☠️ Fim de Jogo! Você perdeu! ☠️");
+        gameOver = true;
+    }
+    if (correctWord.length === chosenWord.length) {
+        // gamewin
+        showEndGameText("win", " 🏆 Parabéns! Você venceu! 🏆");
+        gameOver = true;
+    }
+}
+
+function addCorrectLetter(i) {
+    correctWord += chosenWord[i].toUpperCase();
+}
+
+function disableKey(key, status) {
+    //Add respective class to visually disable key
+    const keysButtons = document.querySelectorAll('.letter');
+    for (let i = 0; i < keysButtons.length; i++) {
+        if (keysButtons[i].textContent === key) {
+            keysButtons[i].classList.add(status);
+            break;
+        }
+    }
+}
+
+function addListeners() {
+    //Adds event listeners to keyboard
+    window.addEventListener('keydown', (e) => {
+        let letter = e.key.toUpperCase();
+        // validation if it's a letter
+        if (keys.toString().includes(letter)) {
+            checkLetter(letter);
+        }
+    });
+
+    //Adds events listeners to virtual keyboard
+    const keysButtons = document.querySelectorAll('.letter');
+    keysButtons.forEach(key => key.addEventListener('click', () => {
+        let letter = key.textContent;
+        checkLetter(letter);
+    }));
+}
